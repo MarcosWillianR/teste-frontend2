ab83541eb494ba048a64d78564519d658dfdef38
Marcos Willian <markusuuuu@gmail.com>
2020-09-16 08:03:18 -0300

    home title done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import { Container, GradientContainer } from './styles';
+import { Container, GradientContainer, TitleContent } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
       <GradientContainer>
         <Header />
+
+        <TitleContent>
+          <h1>Viaje pelo mundo inteiro</h1>
+          <h2>Aqui você encontra os melhores voos do mundo</h2>
+        </TitleContent>
       </GradientContainer>
     </Container>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
-import { GRADIENT_BACKGROUND } from '../../styles/variables';
+import {
+  GRADIENT_BACKGROUND,
+  DESKTOP_FONT_SIZE_64,
+  MOBILE_FONT_SIZE_48,
+  DESKTOP_FONT_SIZE_30,
+  MOBILE_FONT_SIZE_24,
+  MAIN_COLOR_LIGHT,
+  SUBTITLE_COLOR,
+} from '../../styles/variables';
+import { container } from '../../styles/mixins';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+`;
 
 export const GradientContainer = styled.div`
   ${GRADIENT_BACKGROUND};
@@ -14,4 +25,39 @@ export const GradientContainer = styled.div`
   /* height: 100%; */
   /* max-height: 547px; */
   height: 547px;
+`;
+
+export const TitleContent = styled.div`
+  ${container};
+  margin-top: 75px;
+  text-align: center;
+
+  @media screen and (max-width: 760px) {
+    margin-top: 42px;
+  }
+
+  h1 {
+    font-size: ${DESKTOP_FONT_SIZE_64};
+    font-weight: 700;
+    line-height: 75px;
+    color: ${MAIN_COLOR_LIGHT};
+    margin-bottom: 16px;
+
+    @media screen and (max-width: 760px) {
+      font-size: ${MOBILE_FONT_SIZE_48};
+      line-height: 56px;
+    }
+  }
+
+  h2 {
+    font-size: ${DESKTOP_FONT_SIZE_30};
+    font-weight: 400;
+    line-height: 35px;
+    color: ${SUBTITLE_COLOR};
+
+    @media screen and (max-width: 760px) {
+      font-size: ${MOBILE_FONT_SIZE_24};
+      line-height: 28px;
+    }
+  }
 `;
